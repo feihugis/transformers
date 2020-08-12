7b803d880cb92f97a35d22d5b2bf1639121a5501
Yu Yan <yuyan2do@gmail.com>
2020-08-12 00:13:31 +0000

    Reduce the cached memory size

--- src/transformers/modeling_bart.py
+++ src/transformers/modeling_bart.py
@@ -643,6 +643,150 @@ class SelfAttention(nn.Module):
         """Input shape: Time(SeqLen) x Batch x Channel"""
         static_kv: bool = self.encoder_decoder_attention
         tgt_len, bsz, embed_dim = query.size()
+        beam_size = 4
+        assert embed_dim == self.embed_dim
+        assert list(query.size()) == [tgt_len, bsz, embed_dim]
+        # get here for encoder decoder cause of static_kv
+        if layer_state is not None:  # reuse k,v and encoder_padding_mask
+            saved_state = layer_state.get(self.cache_key, {})
+            if "prev_key" in saved_state and static_kv:
+                # previous time steps are cached - no need to recompute key and value if they are static
+                key = None
+        else:
+            saved_state = None
+            layer_state = {}
+
+        q = self.q_proj(query) * self.scaling
+        if static_kv:
+            if key is None:
+                k = v = None
+            else:
+                k = self.k_proj(key)
+                v = self.v_proj(key)
+        else:
+            k = self.k_proj(query)
+            v = self.v_proj(query)
+
+        q = self._shape(q, tgt_len, bsz)
+        if k is not None:
+            k = self._shape(k, -1, bsz)
+        if v is not None:
+            v = self._shape(v, -1, bsz)
+
+        if saved_state is not None:
+            k, v, key_padding_mask = self._use_saved_state(k, v, saved_state, key_padding_mask, static_kv, bsz)
+
+        # Update cache
+        if self.cache_key == 'self':
+            layer_state[self.cache_key] = {
+                "prev_key": k.view(bsz, self.num_heads, -1, self.head_dim),
+                "prev_value": v.view(bsz, self.num_heads, -1, self.head_dim),
+                "prev_key_padding_mask": key_padding_mask if not static_kv else None,
+            }
+
+            assert k is not None
+            src_len = k.size(1)
+            attn_weights = torch.bmm(q, k.transpose(1, 2))
+            assert attn_weights.size() == (bsz * self.num_heads, tgt_len, src_len)
+
+        if self.cache_key == 'encoder_decoder':
+            if self.cache_key not in layer_state:
+                k = k.view(bsz // beam_size, beam_size, self.num_heads, -1, self.head_dim)[:, 0:1, :, :, :].contiguous()
+                v = v.view(bsz // beam_size, beam_size, self.num_heads, -1, self.head_dim)[:, 0:1, :, :, :].contiguous()
+            layer_state[self.cache_key] = {
+                "prev_key": k,
+                "prev_value": v,
+                "prev_key_padding_mask": key_padding_mask if not static_kv else None,
+            }
+
+            assert k is not None
+            src_len = k.size(3)
+            q0 = q.view(bsz // beam_size, beam_size, self.num_heads, tgt_len, self.head_dim)
+            # k0 = k.expand(bsz // beam_size, beam_size, self.num_heads, src_len, self.head_dim)
+            attn_weights = torch.einsum("bmhtd,bnhsd->bmhts", q0, k).reshape(-1, tgt_len, src_len)
+            # k = k.expand(bsz // beam_size, beam_size, self.num_heads, src_len, self.head_dim).reshape(-1, src_len, self.head_dim)
+            # attn_weights = torch.bmm(q, k.transpose(1, 2))
+            
+            assert attn_weights.size() == (bsz * self.num_heads, tgt_len, src_len)
+
+        #print("cache_key: {}, src_len: {}, tgt_len: {}".format(self.cache_key, src_len, tgt_len)) 
+        
+        if attn_mask is not None:
+            attn_weights = attn_weights.view(bsz, self.num_heads, tgt_len, src_len) + attn_mask
+            attn_weights = attn_weights.view(bsz * self.num_heads, tgt_len, src_len)
+
+        # This is part of a workaround to get around fork/join parallelism not supporting Optional types.
+        if key_padding_mask is not None and key_padding_mask.dim() == 0:
+            key_padding_mask = None
+        assert key_padding_mask is None or key_padding_mask.size()[:2] == (bsz, src_len,)
+
+        if key_padding_mask is not None:  # don't attend to padding symbols
+            attn_weights = attn_weights.view(bsz, self.num_heads, tgt_len, src_len)
+            reshaped = key_padding_mask.unsqueeze(1).unsqueeze(2)
+            attn_weights = attn_weights.masked_fill(reshaped, float("-inf"))
+            attn_weights = attn_weights.view(bsz * self.num_heads, tgt_len, src_len)
+        attn_weights = F.softmax(attn_weights, dim=-1)
+        attn_probs = F.dropout(attn_weights, p=self.dropout, training=self.training,)
+
+        assert v is not None
+        if static_kv:
+            attn_probs = attn_probs.view(bsz // beam_size, beam_size, self.num_heads, tgt_len, src_len)
+            # v = v.expand(bsz // beam_size, beam_size, self.num_heads, src_len, self.head_dim).reshape(-1, src_len, self.head_dim)
+            attn_output = torch.einsum("bmhts,bnhsd->bmhtd", attn_probs, v).reshape(-1, tgt_len, self.head_dim)
+        else:
+            attn_output = torch.bmm(attn_probs, v)
+        assert attn_output.size() == (bsz * self.num_heads, tgt_len, self.head_dim)
+        attn_output = attn_output.transpose(0, 1).contiguous().view(tgt_len, bsz, embed_dim)
+        attn_output = self.out_proj(attn_output)
+        if output_attentions:
+            attn_weights = attn_weights.view(bsz, self.num_heads, tgt_len, src_len)
+        else:
+            attn_weights = None
+        return attn_output, attn_weights
+
+    def _use_saved_state(self, k, v, saved_state, key_padding_mask, static_kv, bsz):
+        # saved states are stored with shape (bsz, num_heads, seq_len, head_dim)
+        if "prev_key" in saved_state:
+            _prev_key = saved_state["prev_key"]
+            if static_kv:
+                k = _prev_key
+            else:
+                assert _prev_key is not None
+                prev_key = _prev_key.view(bsz * self.num_heads, -1, self.head_dim)
+                assert k is not None
+                k = torch.cat([prev_key, k], dim=1)
+        if "prev_value" in saved_state:
+            _prev_value = saved_state["prev_value"]
+            assert _prev_value is not None
+            if static_kv:
+                v = _prev_value
+            else:
+                prev_value = _prev_value.view(bsz * self.num_heads, -1, self.head_dim)
+                assert v is not None
+                v = torch.cat([prev_value, v], dim=1)
+        assert k is not None and v is not None
+        prev_key_padding_mask: Optional[Tensor] = saved_state.get("prev_key_padding_mask", None)
+        if prev_key_padding_mask is not None:
+            if static_kv:
+                new_key_padding_mask = prev_key_padding_mask
+            else:
+                new_key_padding_mask = torch.cat([prev_key_padding_mask, key_padding_mask], dim=1)
+        else:
+            new_key_padding_mask = key_padding_mask
+        return k, v, new_key_padding_mask
+    
+    def forward2(
+        self,
+        query,
+        key: Optional[Tensor],
+        key_padding_mask: Optional[Tensor] = None,
+        layer_state: Optional[Dict[str, Optional[Tensor]]] = None,
+        attn_mask: Optional[Tensor] = None,
+        output_attentions=False,
+    ) -> Tuple[Tensor, Optional[Tensor]]:
+        """Input shape: Time(SeqLen) x Batch x Channel"""
+        static_kv: bool = self.encoder_decoder_attention
+        tgt_len, bsz, embed_dim = query.size()
         assert embed_dim == self.embed_dim
         assert list(query.size()) == [tgt_len, bsz, embed_dim]
         # get here for encoder decoder cause of static_kv
@@ -714,8 +858,8 @@ class SelfAttention(nn.Module):
         else:
             attn_weights = None
         return attn_output, attn_weights
-
-    def _use_saved_state(self, k, v, saved_state, key_padding_mask, static_kv, bsz):
+    
+    def _use_saved_state2(self, k, v, saved_state, key_padding_mask, static_kv, bsz):
         # saved states are stored with shape (bsz, num_heads, seq_len, head_dim)
         if "prev_key" in saved_state:
             _prev_key = saved_state["prev_key"]
